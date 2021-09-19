792693894382f1379a96d3e9bf933ec4b6106db1
coderaditisingh <86613587+coderaditisingh@users.noreply.github.com>
2021-09-19 14:46:14 +0530

    save() set timer

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var SpeechRecognition = window.webkitSpeechRecognition;
 var recognition= new SpeechRecognition();
+var Textbox = document.getElementById("text_box");
 
 function start()
 {
@@ -11,18 +12,32 @@ recognition.onresult = function run (event)
 {
     console.log(event);
     var content = event.results[0][0].transcript;
-    console.log(content);
-
     document.getElementById("text_box").innerHTML = content;
-     speak();
+    
+    if(content=="cheese")
+    {
+        console.log("cheese --- ");
+        speak();
+    }   
 }
+
+
 
 function speak()
 {
     var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "taking your selfie in 5 seconds";
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
+    Webcam.attach(camera);
+
+    setTimeout(function()
+{
+    take_snapshot();
+    save();
+},5000);
+
+
 }
 
 Webcam.set(
@@ -34,5 +49,20 @@ Webcam.set(
 });
 
 camera=document.getElementById("camera");
-Webcam.attach(camera);
 
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri)
+    {
+      document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+    }
+    );
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
